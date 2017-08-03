dc3c7d16cfe1070eb876be86a9b791408f029ca5
amit-bhatnagar1995 <anku10264@gmail.com>
2017-08-03 14:34:38 +0530

    Add files via upload

---
+++ cs50/pset6 mario.py
@@ -0,0 +1,23 @@
+import cs50
+
+def main():
+    while True:
+        print("Height: ", end = "")
+        height = cs50.get_int()
+        if height >= 0 and height <= 23:
+            break
+        
+    for i in range(height):
+        for j in range(height - i - 1):
+            print(" ", end = "")
+        for k in range(i+1):
+            print("#", end = "")
+        for t in range(0,1):
+            print(" ", end = "")
+        for l in range(i+1):
+            print("#", end = "")
+        print ("")
+        
+            
+if __name__ == "__main__":
+    main()
